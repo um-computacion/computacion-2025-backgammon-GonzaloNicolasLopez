22429bb29bbde7f7185044f477fe364d86b326c2
GonzaloNicolasLopez <gng.lopez@alumno.um.edu.ar>
2025-09-16 20:38:18 -0300

    agrego metodos y test clase backgammon

--- test/test_backgammon.py
+++ test/test_backgammon.py
@@ -15,23 +15,28 @@ class TestBackgammon(unittest.TestCase):
 
 
     def test_jugador1(self):
-        self.game = Backgammon("Gonzalo", "Pedro")
-        jugador1 = self.game.jugador1()
+        game = Backgammon("Gonzalo", "Pedro")
+        jugador1 = game.jugador1()
         self.assertIsInstance(jugador1, Player)
         self.assertEqual(jugador1.tener_nombre(), "Pedro")
         self.assertEqual(jugador1.tener_color(), "Negras")
 
     def test_jugador2(self):
-        self.game = Backgammon("Gonzalo", "Pedro")
-        jugador2 = self.game.jugador2()
+        game = Backgammon("Gonzalo", "Pedro")
+        jugador2 = game.jugador2()
         self.assertIsInstance(jugador2, Player)
         self.assertEqual(jugador2.tener_nombre(), "Gonzalo")
         self.assertEqual(jugador2.tener_color(), "Blancas")
 
     def test_tablero(self):
-        self.game = Backgammon("Gonzalo", "Pedro")
-        tablero = self.game.tablero()
+        game = Backgammon("Gonzalo", "Pedro")
+        tablero = game.tablero()
         self.assertIsInstance(tablero, Board)
+
+    def tirar_dados(self):
+        game = Backgammon("Gonzalo", "Pedro")
+        game.dados_tirados()
+        self.assertTrue(game.dados_tirados(),all(1 <= x <= 6 for x in game.dados_tirados()))
 
 
 if __name__ == "__main__":
